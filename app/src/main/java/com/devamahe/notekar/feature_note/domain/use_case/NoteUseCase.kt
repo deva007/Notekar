76554184e25236e1ee604fc3acc4a0ee22112035
devamahe <devashish.mountblue@embibe.com>
2022-03-16 06:18:55 +0530

    All other composable and edit note screen

--- app/src/main/java/com/devamahe/notekar/feature_note/domain/use_case/NoteUseCase.kt
+++ app/src/main/java/com/devamahe/notekar/feature_note/domain/use_case/NoteUseCase.kt
@@ -3,5 +3,6 @@ package com.devamahe.notekar.feature_note.domain.use_case
 data class NoteUseCase(
     val getNotes: GetNotes,
     val deleteNote: DeleteNote,
-    val addNote: AddNote
+    val addNote: AddNote,
+    val getNote: GetNote
 )
